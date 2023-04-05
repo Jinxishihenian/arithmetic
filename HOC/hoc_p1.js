2b50a91f095e7198fd7161810ba9df6d4ec47fa8
wss <528890686@qq.com>
2023-04-05 23:06:11 +0800

    在类中this的指向. 函数组合.

---
+++ HOC/hoc_p1.js
@@ -0,0 +1,55 @@
+/*我想知道一些关于这个的事情：
+本班平均成绩
+男生平均成绩
+女生平均成绩
+男生中音调较高
+女生中音调较高*/
+// 这是不是更类似于函数组合.
+// 高阶函数.
+let grades = [
+    { name: 'John', grade: 8, sex: 'M' },
+    { name: 'Sarah', grade: 12, sex: 'F' },
+    { name: 'Bob', grade: 16, sex: 'M' },
+    { name: 'Johnny', grade: 2, sex: 'M' },
+    { name: 'Ethan', grade: 4, sex: 'M' },
+    { name: 'Paula', grade: 18, sex: 'F' },
+    { name: 'Donald', grade: 5, sex: 'M' },
+    { name: 'Jennifer', grade: 13, sex: 'F' },
+    { name: 'Courtney', grade: 15, sex: 'F' },
+    { name: 'Jane', grade: 9, sex: 'F' }
+];
+
+// 是否是女孩.
+let isBoys = student => student.sex === 'M';
+
+// 是否是男孩.
+let isGirl = student => student.sex === 'F';
+
+// 获取所有男孩.
+let getBoys = grades => (grades.filter(isBoys));
+
+// 获取所有女孩.
+let getGirl = grades => (grades.filter(isGirl));
+
+// 获取平均值.
+let average = grades => (
+    grades.reduce((acc, curr) => {
+        return acc + curr;
+    }, 0) / grades.length
+)
+
+// 获取最大值.
+let maxGrade = grads => (Math.max(...grads.map(student => student.grade)));
+
+// 获取最小值.
+let minGrade = grads => (Math.min(...grads.map(student => student.grade)));
+
+// 获取本班的平均值.
+let classroomAverage = average(grades);
+
+// 获取男生平均成绩.
+let boysAverage = average(getBoys(grades));
+
+// 获取女生平均成绩.
+let girlsAverage = average(getGirl(grades));
+
