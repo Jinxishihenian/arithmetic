bf3896d9caadabf06250c1b0769412fd8fe714e8
wss <528890686@qq.com>
2024-12-20 08:33:04 +0800

    JSON案例

---
+++ Xstate/src/demo.ts
@@ -0,0 +1,117 @@
+const steps = {
+    "1": {
+        children: [
+            {
+                "1-1": {
+                    children: [],
+                    states: {
+                        "未激活": {
+                            running: false,
+                        },
+                        "待交互": {
+                            running: false,
+                        },
+                        "进行中": {
+                            running: true,
+                            actions: [
+                                {describe: "选中留置针", behaviorStatus: BehaviorStatus.Running},
+                                {describe: "选中棉签", behaviorStatus: BehaviorStatus.UnStart},
+                                {describe: "选中碘伏", behaviorStatus: BehaviorStatus.UnStart},
+                            ],
+                        },
+                        "完成": {
+                            running: false,
+                        },
+                        "失败": {
+                            running: false,
+                        },
+                    }
+                }
+            }
+        ],
+        states: {
+            "未激活": {
+                running: false,
+                actions: [],
+            },
+            "待交互": {
+                running: false,
+            },
+            "进行中": {
+                running: false,
+                actions: [],
+            },
+            "完成": {
+                running: false,
+                actions: [],
+            },
+            "失败": {
+                running: false,
+                actions: [],
+            },
+        },
+    },
+    "2": {
+        children: [
+            {
+                "1-1": {
+                    children: [],
+                    states: {
+                        "未激活": {
+                            running: false,
+                        },
+                        "待交互": {
+                            running: false,
+                        },
+                        "进行中": {
+                            running: true,
+                            actions: [
+                                {describe: "选中输液泵", behaviorStatus: BehaviorStatus.Running},
+                                {describe: "选中输液架", behaviorStatus: BehaviorStatus.UnStart},
+                            ],
+                        },
+                        "完成": {
+                            running: false,
+                        },
+                        "失败": {
+                            running: false,
+                        },
+                    }
+                }
+            }
+        ],
+        states: {
+            "未激活": {
+                running: false,
+                actions: [],
+            },
+            "待交互": {
+                running: false,
+            },
+            "进行中": {
+                running: false,
+                actions: [],
+            },
+            "完成": {
+                running: false,
+                actions: [],
+            },
+            "失败": {
+                running: false,
+                actions: [],
+            },
+        },
+    },
+    "3": {},
+}
+
+const enum BehaviorStatus {
+    // 未开始.
+    UnStart,
+    // 进行中.
+    Running,
+    // 已完成.
+    Finished,
+    // 已失败.
+    Failed,
+}
